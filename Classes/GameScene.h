bfcc8c33bc00818636beee53048c4bf35e6a0df0
Cintych930 <155135476+cintychens@users.noreply.github.com>
2024-12-22 13:07:59 +0800

    更新 GameScene.h逻辑

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -6,16 +6,21 @@
 
 class GameScene : public cocos2d::Scene {
 public:
-    static GameScene* createScene();
-    virtual bool init();
-    void update(float dt); 
+    static GameScene* createScene(); // 创建场景
+    virtual bool init();             // 初始化场景
+    void update(float dt);           // 每帧更新
 
     CREATE_FUNC(GameScene);
 
 private:
-    CommunitySystem communitySystem; // 管理社交系统
-    std::string getDayFromTime(); // 根据时间获取当前游戏中的日期
-    std::string currentDate;         // 当前日期
+    CommunitySystem communitySystem;  // 社区系统管理器
+    std::string currentDate;          // 当前日期
+
+    // 私有方法
+    void initializeCommunity();      // 初始化社区居民和节日
+    void updateGameDay();            // 更新游戏时间和事件
+    void updateTaskProgress();       // 更新任务进度
+    std::string getDayFromTime();    // 根据时间获取当前游戏中的日期
 };
 
 #endif // GAMESCENE_H
